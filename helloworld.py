c982feada69a7415b1694e765b76f274606a1e8c
diegomartinezdeaspe <92082916+diegomartinezdeaspe@users.noreply.github.com>
2021-11-03 19:27:03 +0100

    dataframe load editing

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,19 @@
-# Hello World
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
 # Check for repositoy functioning
-
-
 print("Hello World")
 
 
+# Check kaggle dataset
+# kaggle datasets download -d rusiano/madrid-airbnb-data
 
+# Connect to kaggle
 
+api = KaggleApi()
+api.authenticate()
+print("Kaggle ready")
 
+df_calendar = pd.read_csv("/Users/diegoma/kaggle/calendar.csv")
+print("pandas dataframe ready")
